23c60961e7e55fd773ab01cb26396cc8de796f7b
Mohammad Valipour <valipour2001@gmail.com>
2024-02-22 19:36:13 +0330

    Exercise 5

---
+++ Chapter1/Exercise5/main.py
@@ -0,0 +1,8 @@
+import math
+
+v = float(input("Input wind speed in km/hour: "))
+t = float(input("Input temperature in degrees Celsius: "))
+
+wci = 13.12 + 0.6215 * t - 11.37 * math.pow(v, 0.16) + 0.3965 * t * math.pow(v, 0.16)
+
+print("The wind chill index is:", int(round(wci, 0)))
