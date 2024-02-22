71dc16dba5faa9a9e178508974d1f97425ea720b
Mohammad Valipour <valipour2001@gmail.com>
2024-02-22 18:48:20 +0330

    Exercise 3

---
+++ Chapter1/Exercise3/main.py
@@ -0,0 +1,9 @@
+PI = 22 / 7  # 3.14
+
+radius = float(input("Radius of Sphere: "))
+
+surArea = 4 * PI * radius ** 2
+volume = (4 / 3) * PI * radius ** 3
+
+print("Surface Area:", surArea)
+print("Volume:", volume)
